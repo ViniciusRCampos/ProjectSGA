9d553f78634f18959be66424388928b41fcd2c4c
Vinicius <vinicius_uo@hotmail.com>
2025-02-03 11:30:37 -0300

    :sparkles: Creation of sale_product migration and model

---
+++ database/migrations/2025_02_03_124108_create_sale_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->foreignId('sale_id')->constrained('sales')->onDelete('restrict');
+            $table->foreignId('product_id')->constrained('products')->onDelete('restrict');
+            $table->integer('quantity');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale__produtcs');
+    }
+};
