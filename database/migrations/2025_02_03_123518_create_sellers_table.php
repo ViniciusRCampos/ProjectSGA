0b3ccd78280be0e8a4439cfd1549c20e7f59a6fd
Vinicius <vinicius_uo@hotmail.com>
2025-02-03 11:29:28 -0300

    :sparkles: Creation of seller migration and model

---
+++ database/migrations/2025_02_03_123518_create_sellers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('CPF')->unique();
+            $table->boolean('active')->default(false);
+            $table->foreignId('store_id')->constrained('stores')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
