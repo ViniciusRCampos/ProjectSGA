09c6293cdf21a0b3d17711957e72e4237475612e
Vinicius <vinicius_uo@hotmail.com>
2025-02-04 01:01:53 -0300

    :sparkles: Added navbar and links

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,27 @@
+<div class="row m-0">
+    <div class="col-12 p-0">
+        <nav class="navbar navbar-expand-sm navbar-light bg-navbar">
+            <a class="navbar-brand" href="#">
+                <img src="SGA-LOGO.jpg" width="45" height="45" class="d-inline-block align-center" alt="">
+                SGA
+            </a>
+            <button class="navbar-toggler border border-secondary text-secondary" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
+                    <path d="M4 6l16 0"></path>
+                    <path d="M4 12l16 0"></path>
+                    <path d="M4 18l16 0"></path>
+                </svg>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav" id="nav_header">
+                    <li class="nav-item" id="nav_vendas">
+                        <a class="nav-link text-dark bold" href="/vendas">Vendas<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item" id="nav_relatorio">
+                        <a class="nav-link text-dark bold" href="/relatorio">Relatorio</a>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
