6e7aae1f8b0219910c2a822099e03357c1a9505c
Vinicius <vinicius_uo@hotmail.com>
2025-02-04 01:01:15 -0300

    :sparkles: Creating the master layout file

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'SGA Project')</title>
+    @vite(['resources/css/app.css'])
+    <style>
+        .bg-navbar {
+            background-color: #94cc83
+        }
+    </style>
+</head>
+@yield('page-style')
+
+<body>
+    @include('layouts.navbar')
+    @yield('content')
+</body>
+@vite(['resources/js/app.js'])
+@yield('page-script')
+
+</html>
