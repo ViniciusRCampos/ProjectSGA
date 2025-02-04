855c5b75f1aba6f665fc24f0a354e7b41ca03666
Vinicius <vinicius_uo@hotmail.com>
2025-02-04 01:13:10 -0300

    :sparkles: Created index function to render the page with field data

---
+++ app/Http/Controllers/SaleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service\SaleService;
+use Illuminate\Http\Request;
+
+class SaleController extends Controller
+{
+    private $saleService;
+
+    public function __construct(SaleService $saleService)
+    {
+        $this->saleService = $saleService;
+    }
+    public function index(){
+        $data = $this->saleService->getData();
+        return view("home")->with("data", $data);
+    }
+}
