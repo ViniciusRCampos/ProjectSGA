78e626c55dee60988450edae95cd74fc369c566c
Vinicius <vinicius_uo@hotmail.com>
2025-02-04 18:48:25 -0300

    :sparkles: Added toastr to the project

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,20 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'SGA Project')</title>
     @vite(['resources/css/app.css'])
+    
     <style>
         .bg-navbar {
             background-color: #94cc83
         }
     </style>
 </head>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
 @yield('page-style')
 
 <body>
     @include('layouts.navbar')
     @yield('content')
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 @vite(['resources/js/app.js'])
 @yield('page-script')
 
