59b5633f694ee2875cb3600412562ff3b20d5461
Vinicius <vinicius_uo@hotmail.com>
2025-02-04 19:34:37 -0300

    :package: Added Jquery Mask Plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './bootstrap';
+import 'jquery-mask-plugin';
+
+window.$ = $;
+window.jQuery = $;
 
 // Código JavaScript personalizado
-$(document).ready(function () {
+$(function () { 
     console.log('jQuery e Bootstrap estão funcionando!');
 });
