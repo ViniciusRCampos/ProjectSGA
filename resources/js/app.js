da44e75bf0026fbb67c7551094c4d52ef11a42d6
Vinicius <vinicius_uo@hotmail.com>
2025-02-04 01:26:51 -0300

    :sparkles: Adjustments for using bootstrap and jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+import $ from 'jquery';
+import 'bootstrap';
 import './bootstrap';
+
+// Código JavaScript personalizado
+$(document).ready(function () {
+    console.log('jQuery e Bootstrap estão funcionando!');
+});
