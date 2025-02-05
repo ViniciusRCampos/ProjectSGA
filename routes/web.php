0ec0720593de5040e1392f91d0686504a0446667
Vinicius <vinicius_uo@hotmail.com>
2025-02-05 20:30:36 -0300

    :sparkles: Routes to edit and find client, seller, store, product

--- routes/web.php
+++ routes/web.php
@@ -29,14 +29,22 @@ Route::get('/', [SaleController::class, "index"])->name('home');
 Route::get('/relatorio', [SaleController::class, "showReport"])->name('getReport');
 
 Route::get('/store/sellers/{storeId}', [SellerController::class, 'getSellers'])->name('getSellers');
-
+Route::get('/seller/{sellerId}', [SellerController::class, 'getSellerById'])->name('getSellerById');
+Route::put('/seller/update/{sellerId}', [SellerController::class, 'updateSeller'])->name('updateSeller');
 Route::post('/store/seller/add', [SellerController::class, 'addNewSeller'])->name('addNewSeller');
 
+
 Route::post('/client/add', [ClientController::class, 'addNewClient'])->name('addClient');
+Route::get('/client/{clientId}', [ClientController::class, 'getClientById'])->name('getClientById');
+Route::put('/client/update/{clientId}', [ClientController::class, 'updateClient'])->name('updateClient');
 
 Route::post('/store/add', [StoreController::class, 'addNewStore'])->name('addStore');
+Route::get('/store/{storeId}', [StoreController::class, 'getStoreById'])->name('getStoreById');
+Route::put('/store/update/{storeId}', [StoreController::class, 'updateStore'])->name('updateStore');
 
 Route::post('/product/add', [ProductController::class, 'addNewProduct'])->name('addProduct');
+Route::get('/product/{productId}', [ProductController::class, 'getProductById'])->name('getProductById');
+Route::put('/product/update/{productId}', [ProductController::class, 'updateProduct'])->name('updateProduct');
 
 Route::post('/sale/register', [SaleOperationController::class, 'registerSale'])->name('registerSale');
 
