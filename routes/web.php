8bca03f2991e4889999aac56573ea91db98e3346
Vinicius <vinicius_uo@hotmail.com>
2025-02-05 09:53:59 -0300

    :sparkles: Route to add new store

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\SaleController;
 use App\Http\Controllers\SellerController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,3 +26,5 @@ Route::get('/', [SaleController::class, "index"])->name('home');
 Route::get('/store/sellers/{storeId}', [SellerController::class, 'getSellers'])->name('getSellers');
 
 Route::post('/client/add', [ClientController::class, 'addNewClient'])->name('addClient');
+
+Route::post('/store/add', [StoreController::class, 'addNewStore'])->name('addStore');
