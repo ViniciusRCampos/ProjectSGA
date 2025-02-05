cecfa1b834fa4e2dc2c9b3db5ecea9ed0129f0a5
Vinicius <vinicius_uo@hotmail.com>
2025-02-05 01:34:40 -0300

    :sparkles: Added payment fields and gender to the function

--- app/Service/SaleService.php
+++ app/Service/SaleService.php
@@ -3,32 +3,50 @@
 namespace App\Service;
 
 use App\Repository\Model\ClientRepository;
+use App\Repository\Model\GenderRepository;
+use App\Repository\Model\PaymentMethodRepository;
 use App\Repository\Model\ProductRepository;
 use App\Repository\Model\SaleRepository;
 use App\Repository\Model\SellerRepository;
 use App\Repository\Model\StoreRepository;
 
-class SaleService {
+class SaleService
+{
 
     private $saleRepository;
     private $storeRepository;
     private $productRepository;
     private $clientRepository;
     private $sellerRepository;
-    public function __construct(SaleRepository $saleRepository, StoreRepository $storeRepository, ProductRepository $productRepository,
-    ClientRepository $clientRepository, SellerRepository $sellerRepository) {
+    private $genderRepository;
+    private $paymentMethodRepository;
+    public function __construct(
+        SaleRepository $saleRepository,
+        StoreRepository $storeRepository,
+        ProductRepository $productRepository,
+        ClientRepository $clientRepository,
+        SellerRepository $sellerRepository,
+        GenderRepository $genderRepository,
+        PaymentMethodRepository $paymentMethodRepository
+    ) {
         $this->saleRepository = $saleRepository;
         $this->storeRepository = $storeRepository;
         $this->productRepository = $productRepository;
         $this->clientRepository = $clientRepository;
         $this->sellerRepository = $sellerRepository;
+        $this->genderRepository = $genderRepository;
+        $this->paymentMethodRepository = $paymentMethodRepository;
+
     }
 
-    public function getData() {
+    public function getData()
+    {
         $data = [
             'clients' => $this->clientRepository->all(),
             'stores' => $this->storeRepository->all(),
             'products' => $this->productRepository->all(),
+            'genders' => $this->genderRepository->getActives(),
+            'paymentMethods' => $this->paymentMethodRepository->getActives(),
         ];
         return $data;
     }
