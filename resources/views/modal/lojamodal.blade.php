fe9e56afa0c0d99c236df1fcd85d60ee1a684572
Vinicius <vinicius_uo@hotmail.com>
2025-02-05 09:53:43 -0300

    :sparkles: Added error messages and ids to necessary elements

--- resources/views/modal/lojamodal.blade.php
+++ resources/views/modal/lojamodal.blade.php
@@ -1,5 +1,5 @@
 <div class="modal" tabindex="-1" id="modal_loja">
-    <div class="modal-dialog modal modal-dialog-centered">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Loja</h5>
@@ -8,7 +8,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <form>
+                <form id="form_modal_loja">
                     <div class="form-row justify-content-end">
                         <div class="custom-control custom-switch d-block">
                             <input type="checkbox" class="custom-control-input" id="switch_loja" checked>
@@ -16,19 +16,25 @@
                         </div>
                     </div>
                     <div class="form-row">
-                        <div class="col-8 form-group ">
+                        <div class="col-7 form-group ">
                             <label for="form_nome_loja">Nome</label>
-                            <input type="text" class="form-control" id="form_nome_loja" name="form_nome_loja">
+                            <input type="text" class="form-control" id="form_nome_loja" name="form_nome_loja" required>
+                            <div class="invalid-feedback">
+                                Digite um nome válido.
+                            </div>
                         </div>
-                        <div class="col-4 form-group ">
+                        <div class="col-5 form-group ">
                             <label for="form_cnpj_loja">CNPJ</label>
-                            <input type="text" class="form-control cnpj" name="form_cnpj_loja" id="form_cnpj_loja">
+                            <input type="text" class="form-control cnpj" name="form_cnpj_loja" id="form_cnpj_loja" required>
+                            <div class="invalid-feedback">
+                                CNPJ inválido! Digite um CNPJ válido.
+                            </div>
                         </div>
 
-                        <div class="col-4 form-group ">
+                        <div class="col-5 form-group ">
                             <label for="form_cep_loja">CEP</label>
                             <div class="input-group">
-                                <input type="text" class="form-control cep" id="form_cep_loja" name="form_cep_loja">
+                                <input type="text" class="form-control cep" id="form_cep_loja" name="form_cep_loja" required>
                                 <div class="input-group-append">
                                     <button class="btn btn-outline-info" id="btn_pesquisa_cep" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-search">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -36,10 +42,13 @@
                                         <path d="M21 21l-6 -6" />
                                     </svg></button>
                                 </div>
+                                <div class="invalid-feedback">
+                                CEP inválido! Digite um CEP válido.
+                            </div>
                             </div>
                         </div>
 
-                        <div class="col-8 form-group ">
+                        <div class="col-7 form-group ">
                             <label for="form_cidade_loja">Cidade</label>
                             <input type="text" readonly disabled class="form-control" name="form_cidade_loja" id="form_cidade_loja">
                         </div>
@@ -56,7 +65,10 @@
 
                         <div class="col-6 form-group ">
                             <label for="form_numero_loja">Numero</label>
-                            <input type="text" class="form-control" id="form_numero_loja" name="form_numero_loja">
+                            <input type="text" class="form-control" id="form_numero_loja" name="form_numero_loja" required>
+                            <div class="invalid-feedback">
+                                Número obrigatorio, caso não tenha coloque S/N
+                            </div>
                         </div>
 
                         <div class="col-8 form-group ">
@@ -74,7 +86,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                <button type="button" class="btn btn-primary">Salvar</button>
+                <button type="button" class="btn btn-primary" id="btn_criar_loja">Salvar</button>
             </div>
         </div>
     </div>
