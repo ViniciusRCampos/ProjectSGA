fd93b615f9a71c01f10393799dcc1863a99a711c
Vinicius <vinicius_uo@hotmail.com>
2025-02-06 00:08:15 -0300

    :sparkles: Functions for finding and updating a product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Service\ProductService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -13,7 +15,13 @@ class ProductController extends Controller
         $this->productService = $productService;
     }
 
-    public function addNewProduct(Request $request){
+    /**
+     * Receives a request and sends it to add a new product
+     * @param \App\Http\Requests\ProductRequest $request
+     * @return JsonResponse
+     */
+    public function addNewProduct(ProductRequest $request): JsonResponse
+    {
         $product = $this->productService->addNewProduct($request->only([
             "name",
             "description",
@@ -21,6 +29,37 @@ class ProductController extends Controller
             "active",
             "price"
         ]));
+        return $product;
+    }
+
+    /**
+     * Search for the product according to the id provided
+     * @param int $productId
+     * @return JsonResponse
+     */
+    public function getProductById(int $productId): JsonResponse
+    {
+        $product = $this->productService->getProductById($productId);
+        return $product;
+    }
+
+    /**
+     * Updates the product data entered
+     * @param \App\Http\Requests\ProductRequest $request
+     * @param int $productId
+     * @return JsonResponse
+     */
+    public function updateProduct(ProductRequest $request,int $productId): JsonResponse
+    {
+        $data = $request->only([
+            "name",
+            "description",
+            "color",
+            "active",
+            "price"
+        ]);
+        $data = array_merge($data, ['id' => $productId]);
+        $product = $this->productService->updateProduct($data);
         return $product;
     }
 }
--- app/Service/ProductService.php
+++ app/Service/ProductService.php
@@ -18,11 +18,47 @@ class ProductService
         $this->responseProvider = $responseProvider;
     }
 
+    /**
+     * Receives an array of data to add a new product
+     * @param array $data
+     * @return JsonResponse
+     */
     public function addNewProduct(array $data): JsonResponse
     {
         try {
             $product = $this->productRepository->create($data);
             return $this->responseProvider->success($product, "Product registered", 201);
+        } catch (\Throwable $e) {
+            Log::error($e->getMessage(), $data);
+            return $this->responseProvider->error($e->getCode());
+        }
+    }
+
+    /**
+     * Search for the product according to the id provided
+     * @param int $productId
+     * @return JsonResponse
+     */
+    public function getProductById(int $productId): JsonResponse
+    {
+        try {
+            $product = $this->productRepository->find($productId);
+            return $this->responseProvider->success($product, '', 200);
+        } catch (\Throwable $e) {
+            Log::error($e->getMessage(), [$productId]);
+            return $this->responseProvider->error($e->getCode());
+        }
+    }
+    /**
+     * Updates product information based on received data
+     * @param array $data
+     * @return JsonResponse
+     */
+    public function updateProduct(array $data): JsonResponse
+    {
+        try {
+            $product = $this->productRepository->update($data['id'], $data);
+            return $this->responseProvider->success($product, '', 200);
         } catch (\Throwable $e) {
             Log::error($e->getMessage(), $data);
             return $this->responseProvider->error($e->getCode());
