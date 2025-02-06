1e4759185267c5ef9cbdc83b5b4cda21c212c2ee
Vinicius <vinicius_uo@hotmail.com>
2025-02-06 00:04:04 -0300

    :sparkles: Functions required to find and update a customer

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ClientRequest;
 use App\Service\ClientService;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class ClientController extends Controller
 {
@@ -13,9 +14,39 @@ class ClientController extends Controller
     {
         $this->clientService = $clientService;
     }
-    public function addNewClient(Request $request) {
-
+    /**
+     * Receives the request to request the registration of a new customer
+     * @param \App\Http\Requests\ClientRequest $request
+     * @return JsonResponse
+     */
+    public function addNewClient(ClientRequest $request): JsonResponse
+    {
         $response = $this->clientService->addNewClient($request->only(["name", "email", "cpf", "genderId", "active"]));
         return $response;
+    }
+
+    /**
+     * Search for a customer based on the id entered
+     * @param int $clientId
+     * @return JsonResponse
+     */
+    public function getClientById(int $clientId): JsonResponse
+    {
+        $client = $this->clientService->getClientById($clientId);
+        return $client;
+    }
+
+    /**
+     * Receives a request with data for updating a client informed by id
+     * @param \App\Http\Requests\ClientRequest $request
+     * @param int $clientId
+     * @return JsonResponse
+     */
+    public function updateClient(ClientRequest $request,int $clientId): JsonResponse
+    {
+        $data = $request->only('name', 'cpf', 'email', 'genderId', 'active');
+        $data = array_merge($data, ['id' => $clientId]);
+        $client = $this->clientService->updateClient($data);
+        return $client;
     }
 }
--- app/Service/ClientService.php
+++ app/Service/ClientService.php
@@ -17,24 +17,67 @@ class ClientService
         $this->clientRepository = $clientRepository;
         $this->responseProvider = $responseProvider;
     }
-
-    private function prepareDataArray(array $data){
+    /**
+     * Organizes data to match a client's fields
+     * @param array $data
+     * @return array{CPF: int, active: boolean, email: string, gender_id: int, name: string}
+     */
+    private function prepareDataArray(array $data)
+    {
         $data = [
             'name' => $data['name'],
-            'CPF'=> $data['cpf'],
-            'gender_id'=> $data['genderId'],
+            'CPF' => $data['cpf'],
+            'gender_id' => $data['genderId'],
             'email' => $data['email'],
-            'active'=> $data['active'],
+            'active' => $data['active'],
         ];
         return $data;
     }
-
-    public function addNewClient(array $data): JsonResponse {
-        try{
+    /**
+     * Function responsible for sending new customer data for registration
+     * @param array $data
+     * @return JsonResponse
+     */
+    public function addNewClient(array $data): JsonResponse
+    {
+        try {
             $newClientData = $this->prepareDataArray($data);
             $client = $this->clientRepository->create($newClientData);
             return $this->responseProvider->success($client, 'Client created with success', 201);
-        } catch (\Throwable $e){
+        } catch (\Throwable $e) {
+            Log::error($e->getMessage(), $data);
+            return $this->responseProvider->error($e->getCode());
+        }
+    }
+
+    /**
+     * Searches for a customer's data according to the id provided
+     * @param int $clientId
+     * @return JsonResponse
+     */
+    public function getClientById(int $clientId): JsonResponse
+    {
+        try {
+            $client = $this->clientRepository->find($clientId);
+            return $this->responseProvider->success($client, '', 200);
+        } catch (\Throwable $e) {
+            Log::error($e->getMessage(), [$clientId]);
+            return $this->responseProvider->error($e->getCode());
+        }
+    }
+
+    /**
+     * Update a customer's data
+     * @param array $data
+     * @return JsonResponse|mixed
+     */
+    public function updateClient(array $data): JsonResponse
+    {
+        try {
+            $clientData = $this->prepareDataArray($data);
+            $client = $this->clientRepository->update($data['id'], $clientData);
+            return $this->responseProvider->success($client, '', 200);
+        } catch (\Throwable $e) {
             Log::error($e->getMessage(), $data);
             return $this->responseProvider->error($e->getCode());
         }
